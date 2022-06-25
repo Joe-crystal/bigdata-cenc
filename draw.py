fabd0be9a762f49b2a18a71bd6e8eb41dc8f21f7
xfhcxxy <2304060191@qq.com>
2022-06-25 16:41:07 +0800

    6个图加上一个简单暴力的查询(地图初始定位在中国)

--- draw.py
+++ draw.py
@@ -5,6 +5,7 @@ import folium
 from folium import plugins
 from folium.plugins import HeatMap
 from pyecharts.charts import Bar
+from pyecharts.charts import Line
 from pyecharts import options as opts
 
 
@@ -23,8 +24,8 @@ def read_data():
             break
         line = line.split(' ')
         m.append(float(line[0]))
-        date.append(line[1])
-        time.append(line[2])
+        date.append(str(line[1]))
+        time.append(str(line[2]))
         lat.append(float(line[3]))
         lon.append(float(line[4]))
         dep.append(float(line[5]))
@@ -60,7 +61,7 @@ def query(date_1='2000-01-01', date_2='3000-01-01', lat_1=-90.0, lat_2=90.0, lon
 '''
 同时画所有点
 '''
-def draw_pot_1(lat, lon):     # 纬度和经度
+def draw_pot_all_pot(lat, lon):     # 纬度和经度
     lat = np.array(lat)
     lon = np.array(lon)
     world_map = folium.Map(location=[35.3, 100.6], zoom_start=4, control_scale=True,)
@@ -77,32 +78,32 @@ def draw_pot_1(lat, lon):     # 纬度和经度
             )
         )
     world_map.add_child(incidents)
-    world_map.save('world_map1.html')
+    world_map.save('./map/pot_all_pot.html')
 
 
 ''' 
 一个区域类的点会合成一个点，点上数字表示这个区域有多少点
 '''
-def draw_pot_2(lat, lon):     # 纬度和经度
+def draw_pot_market_pot(lat, lon):     # 纬度和经度
     lat = np.array(lat)
     lon = np.array(lon)
     world_map = folium.Map(location=[35.3, 100.6], zoom_start=4, control_scale=True,)
     marker_cluster = plugins.MarkerCluster().add_to(world_map)
     for i in range(len(lat)):
         folium.Marker(location=[lat[i], lon[i]]).add_to(marker_cluster)
-    world_map.save('world_map2.html')
+    world_map.save('./map/pot_market_pot.html')
 
 
 '''
 静态热力图
 '''
-def draw_heat_map_1(m, lat, lon):
+def draw_heat_map_static(m, lat, lon):
     data = []
     for i in range(len(m)):
         data.append([lat[i], lon[i], m[i]])
     world_map = folium.Map(location=[35.3, 100.6], zoom_start=4, control_scale=True,)
     HeatMap(data).add_to(world_map)
-    world_map.save("HeatMap1.html")
+    world_map.save("./map/heat_map_static.html")
 
 
 '''
@@ -111,7 +112,7 @@ t = y 按年划分
 t = m 按月划分（默认）
 t = d 按日划分
 '''
-def draw_heat_map_2(m, date, lat, lon, t='m'):
+def draw_heat_map_dynamic(m, date, lat, lon, t='m'):
     data = []       # 总数据
     data_m = []     # 一个时间划分的数据
     date_m = []
@@ -129,13 +130,13 @@ def draw_heat_map_2(m, date, lat, lon, t='m'):
 
     world_map = folium.Map(location=[35.3, 100.6], zoom_start=4, control_scale=True, )
     plugins.HeatMapWithTime(data, index=date_m).add_to(world_map)
-    world_map.save("HeatMap2.html")
+    world_map.save("./map/heat_map_dynamic.html")
 
 
 '''
 柱状图 震级-次数
 '''
-def draw_bar_1(m):
+def draw_bar_m_to_num(m):
     bar = Bar()
     count = {}
     for i in m:
@@ -150,15 +151,47 @@ def draw_bar_1(m):
         y.append(count[x])
     bar.add_yaxis("地震次数", y)
     bar.set_global_opts(title_opts=opts.TitleOpts(title="震级-次数", subtitle="四川大学"))
-    bar.render("bar1.html")
+    bar.render("./map/bar_m_to_num.html")
+
+'''
+时间-地震次数折线图
+t = y 按年划分
+t = m 按月划分（默认）
+t = d 按日划分
+'''
+def draw_line_time_to_num(date, t='m'):
+    line = Line()
+    lim = 7
+    if t == 'd':
+        lim = 10
+    elif t == 'y':
+        lim = 4
+    for i in range(len(date)):
+        date[i] = date[i][:lim]
+    date_dict = {}
+    for d in date:
+        if d not in date_dict:
+            date_dict[d] = 0
+        date_dict[d] += 1
+    date = list(set(date))
+    date.sort()
+    line.add_xaxis(date)
+    y = []
+    for d in date:
+        y.append(date_dict[d])
+    line.add_yaxis("地震次数", y)
+    line.set_global_opts(title_opts=opts.TitleOpts(title="时间-次数", subtitle="四川大学"))
+    line.render("./map/line_time_to_num.html")
+
+
 
 
 if __name__ == "__main__":      # 用已经爬的全部数据演示
     m, date, time, lat, lon, dep, loc = read_data()
-    draw_pot_1(lat, lon)
-    draw_pot_2(lat, lon)
-    draw_heat_map_1(m, lat, lon)
+    #draw_pot_all_pot(lat, lon)
+    #draw_pot_market_pot(lat, lon)
+    #draw_heat_map_static(m, lat, lon)
     #m, date, time, lat, lon, dep, loc = query(date_1='2020-01-01', date_2='2021-01-01')
-    draw_heat_map_2(m, date, lat, lon, t='m')
-    draw_bar_1(m)
-
+    #draw_heat_map_dynamic(m, date, lat, lon, t='m')
+    draw_bar_m_to_num(m)
+    draw_line_time_to_num(date, t='m')
